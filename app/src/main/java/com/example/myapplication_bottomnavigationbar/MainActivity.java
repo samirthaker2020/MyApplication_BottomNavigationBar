41aed661dd0704fd60f10931830cbea2e9af9db8
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2020-09-03 18:14:04 -0400

    Added bottom nav bar with fragements

--- app/src/main/java/com/example/myapplication_bottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/myapplication_bottomnavigationbar/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.myapplication_bottomnavigationbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener( navigationItemSelectedListener);
+        openFragment(HomeFragment.newInstance("", ""));
+
+    }
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.navigation_home:
+                            openFragment(HomeFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_sms:
+                            openFragment(SmsFragment.newInstance("", ""));
+                            return true;
+                        case R.id.navigation_notifications:
+                            openFragment(NotificationFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return false;
+                }
+            };
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
